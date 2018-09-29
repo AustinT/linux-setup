a43cd122e8c2225e856a5bcca5fc783f480631bf
Austin T <zzz>
2018-09-29 19:03:10 -0400

    Added code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # linux-setup
 Shell scripts to set up a new Linux computer the way I like it.
+Currently only tested on Ubuntu linux.
+I don't recommend you run it without looking at what it does.
+Hope you all enjoy!
---
+++ linux_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Note: this script includes some sudos,
+# Because I don't want the whole thing to be run as sudo
+# Consider installing the following manually
+# anaconda, dropbox, zotero
+# Also add ssh keys to github
+
+# Enter my email to make configurations easier
+if [[ -z "$1" ]] ; then
+    echo 'Email argument not provided. Aborting!'
+    exit 1
+fi
+
+echo "INSTALLING PROGRAMS.."
+sudo apt-get dist-upgrade
+sudo apt-get upgrade
+sudo apt-get -y install vim git redshift anki tmux curl
+source install_chrome.sh
+
+echo "CONFIGURING PROGRAMS..."
+# Add my git stuff
+git config --global user.name "Austin T."
+git config --global user.email "$1"
+
+# Add my vim rc file
+git clone https://github.com/AustinT/my_vimrc.git
+mv ./my_vimrc/.vimrc ~/
+source ./my_vimrc/setup_vim_plugins.sh
+rm -rf my_vimrc/
+
+echo "GENERATING SSH KEYS..."
+ssh-keygen -t rsa -b 4096 -C "$1"
+# add to ssh agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
