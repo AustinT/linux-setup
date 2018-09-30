dbfdc46e35e931afe84991a76704ac730683f3af
Austin T <austin.james.tripp@gmail.com>
2018-09-30 10:46:16 -0400

    Modified python installation to use python 3.6, since 3.7 isn't properly supported by tensorflow yet

--- python_conda_packages.sh
+++ python_conda_packages.sh
@@ -1,8 +1,12 @@
 conda update -n base conda
 
+# Python 3.7 isn't fully supported yet... so plz 3.6
+conda install python=3.6
+
 # Standard data processing libraries
 conda install -y numpy scipy matplotlib scikit-learn pandas jupyter jupyterlab ipython sympy seaborn imageio 
 
 # Machine learning libraries (assuming no gpu)
-pip install tensorflow keras
+pip install -U pip
+pip install -U tensorflow keras tensorboard
 conda install pytorch-cpu torchvision-cpu -c pytorch
