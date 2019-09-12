d116b592369dd51a93c51aca34cbceadf5bbe3da
Austin Tripp <tripp.aust@gmail.com>
2019-09-12 18:40:01 -0400

    Added bashrc addition file

---
+++ bashrc_additions.sh
@@ -0,0 +1,6 @@
+# Vim colors
+if [ -f ~/.vim/bundle/vim-solarized8/scripts/solarized8.sh ]; then
+    bash ~/.vim/bundle/vim-solarized8/scripts/solarized8.sh
+else
+    echo "WARNING: solarized colorscheme not installed correctly"
+fi
