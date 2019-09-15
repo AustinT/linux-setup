ca2c62ce83c6c066a0498a569362ec242120e002
Austin Tripp <tripp.aust@gmail.com>
2019-09-15 12:43:16 -0400

    Added tmux dir

--- bashrc_additions.sh
+++ bashrc_additions.sh
@@ -1,3 +1,7 @@
+# Tmux stuff
+export TMUX_TMPDIR=$HOME/.tmux/tmp-dir
+mkdir -p "$TMUX_TMPDIR"
+
 # Vim colors for fixing issue with truecolor terminals
 if [ -f ~/.vim/bundle/vim-solarized8/scripts/solarized8.sh ]; then
     bash ~/.vim/bundle/vim-solarized8/scripts/solarized8.sh
