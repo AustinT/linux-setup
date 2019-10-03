79e756302ea32f0202e13b66e424c73e975585d1
Austin T <austin.james.tripp@gmail.com>
2019-10-03 14:43:21 +0100

    Updated syntax to use "cd -"

--- setup_vim_plugins.sh
+++ setup_vim_plugins.sh
@@ -4,7 +4,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install pathogen plugins
-orig_dir=$(pwd)
 cd ~/.vim/bundle
 
 # Supertab
@@ -17,4 +16,5 @@ git clone --recurse-submodules https://github.com/python-mode/python-mode.git
 git clone https://github.com/lifepillar/vim-solarized8.git
 echo "Remember to add colorscheme script to bashrc"
 
-cd $orig_dir
+# Back to original directory
+cd -
